797b2c404175dec4dcdc9c438afed68994f24d8e
Cristian Petre <driimus@vivaldi.net>
2024-09-22 20:37:28 +0100

    test: transform objects

---
+++ src/index.test.ts
@@ -0,0 +1,44 @@
+import { randomInt, randomUUID } from "crypto";
+import { describe, expect, it } from "vitest";
+import { xf } from "./index.js";
+
+describe("xf", async () => {
+    describe('#define', () => {
+        it('should combine inputs into a new transformer', () => {
+            type Input = Record<string, (obj: {
+                name: string;
+                age: number;
+            }) => any>;
+
+            const a = { id() { return '1'; } } satisfies Input;
+            const b = { parent(obj) { return { id: obj.name }; } } satisfies Input;
+
+            const transformer = xf.define(a, b)
+
+            expect(Object.getOwnPropertyNames(a)).toStrictEqual(['id'])
+            expect(Object.getOwnPropertyNames(b)).toStrictEqual(['parent'])
+            expect(Object.getOwnPropertyNames(transformer)).toStrictEqual(['id', 'parent'])
+        })
+    })
+
+    describe('#toObject', () => {
+        it('should transform the input object', () => {
+            const obj = {
+                firstName: 'C',
+                lastName: 'P',
+                id: randomUUID(),
+                age: randomInt(100)
+            }
+
+            const out = xf.toObject({
+                doubleAge(obj) { return obj.age * 2 },
+                fullName(obj) { return [obj.firstName, obj.lastName].join(' ') }
+            }, obj)
+            
+            expect(out).toStrictEqual({
+                doubleAge: obj.age * 2,
+                fullName: `${obj.firstName} ${obj.lastName}`
+            })
+        })
+    })
+})
