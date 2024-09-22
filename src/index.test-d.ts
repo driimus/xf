e85f907fede749b76b99b7afc5d7c0da420be62a
Cristian Petre <driimus@vivaldi.net>
2024-09-22 20:59:33 +0100

    style: enable biome

--- src/index.test-d.ts
+++ src/index.test-d.ts
@@ -1,40 +1,58 @@
-import { randomInt, randomUUID } from "crypto";
+import { randomInt, randomUUID } from "node:crypto";
 import { describe, expectTypeOf, it } from "vitest";
 import { xf } from "./index.js";
 
 describe("xf", async () => {
-    describe('#define', () => {
-        it('should combine inputs into a new transformer', () => {
-            type Input = Record<string, (obj: {
-                name: string;
-                age: number;
-            }) => any>;
+  describe("#define", () => {
+    it("should combine inputs into a new transformer", () => {
+      type Input = Record<
+        string,
+        (obj: {
+          name: string;
+          age: number;
+        }) => unknown
+      >;
 
-            const a = { id() { return '1'; } } satisfies Input;
-            const b = { parent(obj) { return { id: obj.name }; } } satisfies Input;
+      const a = {
+        id() {
+          return "1";
+        },
+      } satisfies Input;
+      const b = {
+        parent(obj) {
+          return { id: obj.name };
+        },
+      } satisfies Input;
 
-            const transformer = xf.define(a, b)
+      const transformer = xf.define(a, b);
 
-            expectTypeOf(transformer).toMatchTypeOf(a)
-            expectTypeOf(transformer).toMatchTypeOf(b)
-        })
-    })
+      expectTypeOf(transformer).toMatchTypeOf(a);
+      expectTypeOf(transformer).toMatchTypeOf(b);
+    });
+  });
 
-    describe('#toObject', () => {
-        it('should transform the input object', () => {
-            const obj = {
-                firstName: 'C',
-                lastName: 'P',
-                id: randomUUID(),
-                age: randomInt(100)
-            }
+  describe("#toObject", () => {
+    it("should transform the input object", () => {
+      const obj = {
+        firstName: "C",
+        lastName: "P",
+        id: randomUUID(),
+        age: randomInt(100),
+      };
 
-            const out = xf.toObject({
-                doubleAge(obj) { return obj.age * 2 },
-                fullName(obj) { return [obj.firstName, obj.lastName].join(' ') }
-            }, obj)
-            
-            expectTypeOf(out).toEqualTypeOf<{doubleAge: number, fullName: string}>()
-        })
-    })
-})
+      const out = xf.toObject(
+        {
+          doubleAge(obj) {
+            return obj.age * 2;
+          },
+          fullName(obj) {
+            return [obj.firstName, obj.lastName].join(" ");
+          },
+        },
+        obj,
+      );
+
+      expectTypeOf(out).toEqualTypeOf<{ doubleAge: number; fullName: string }>();
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,43 @@
 type Simplify<T> = {
-  [K in keyof T]: T[K]
+  [K in keyof T]: T[K];
 };
 
 export type PropertyTransformer<In = unknown, Out = unknown> = Record<
   string,
   <T extends In>(data: T) => Out
->
+>;
 
 type TransformOutput<T extends PropertyTransformer> = Simplify<{
-  [K in keyof T]: ReturnType<T[K]>
-}>
+  [K in keyof T]: ReturnType<T[K]>;
+}>;
 
 type Entry<T extends PropertyTransformer> = [keyof T, T[keyof T]];
 
-const entries = Object.entries as <T extends PropertyTransformer>(obj: T) => Entry<T>[]
+const entries = Object.entries as <T extends PropertyTransformer>(obj: T) => Entry<T>[];
 
 function toObject<TIn, TOut, T extends PropertyTransformer<TIn, TOut>>(
   transformer: T,
   data: TIn,
 ): TransformOutput<T> {
-  const out: Partial<TransformOutput<T>> = {}
+  const out: Partial<TransformOutput<T>> = {};
 
   for (const [property, transform] of entries(transformer))
-    out[property] = transform(data) as typeof out[typeof property]
+    out[property] = transform(data) as (typeof out)[typeof property];
 
-  return out as TransformOutput<T>
+  return out as TransformOutput<T>;
 }
 
 function toValues<TIn, TOut, T extends PropertyTransformer<TIn, TOut>>(
   transformer: T,
   data: TIn,
 ): TransformOutput<T>[keyof T][] {
-  return Object.values(toObject(transformer, data))
+  return Object.values(toObject(transformer, data));
 }
 
-const define: typeof Object.assign = (...args: unknown[]) => Object.assign({}, ...args)
+const define: typeof Object.assign = (...args: unknown[]) => Object.assign({}, ...args);
 
 export const xf = {
   define,
   toObject,
   toValues,
-}
-
+};
